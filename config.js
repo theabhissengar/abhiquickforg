1628d961177eb5cf2a5ce0a4bf99cc8808058cad
theabhissengar <theabhissengar@gmail.com>
2025-04-09 13:28:33 +0530

    ff

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-  baseUrl: process.env.NEXT_PUBLIC_BASE_URL || 'https://quicktest.vercel.app'
+  baseUrl: process.env.NEXT_PUBLIC_BASE_URL || 'https://abhiquickforg.vercel.app'
 };
 
 export default config; 
