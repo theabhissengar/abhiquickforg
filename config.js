3116e8df7cb89140e1d5c67400b98fce41dab6b3
theabhissengar <theabhissengar@gmail.com>
2025-04-09 13:33:06 +0530

    fff

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-  baseUrl: process.env.NEXT_PUBLIC_BASE_URL || 'https://abhiquickforg.vercel.app'
+  baseUrl: process.env.NEXT_PUBLIC_BASE_URL || 'https://quickforgetclone.vercel.app'
 };
 
 export default config; 
